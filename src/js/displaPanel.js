4950c792be92d10bbd8b82a735dfc68ab88f7538
Stefan-Gabriel Muscalu <stefan@findie.me>
2017-11-11 19:16:33 +0200

    Fix: choppy code highlight Add: .gitignore

--- src/js/displaPanel.js
+++ src/js/displaPanel.js
@@ -25,21 +25,56 @@ const findLineCharFromIndex = (string, index) => {
   return { line, ch };
 };
 const walkCodeAndGetStartEnd = (code, path) => {
+  console.clear();
   let find = code;
-  let index = 0;
+  let codeIndex = 0;
 
   path.forEach(p => {
 
-    const exp = (new RegExp(`\{?("|')?${p}("|')?\s*?\:\s*?((.*?)(,|\\}|\\n|$))`, 'g')).exec(find);
-    // console.log(exp);
-    index += exp.index;
-    find = exp[ 0 ];
-    // console.log(find);
+    console.log(find);
+    const reg = (new RegExp(`\{?("|')?${p}("|')?\s*?\:[\u0000-\uffff]*?(\\{|,|'|"|t|f|n|u|\\d)`, 'g')).exec(find);
+
+    let index = reg.index - 1;
+    codeIndex += index;
+
+    find = find.substr(index);
+
+    const start = index;
+    let deep = 0;
+    let lastMeaningfulIndex = index;
+
+    while (deep >= 0 && index < code.length) {
+       const c = code.charAt(index);
+
+      if (deep === 0 && c === ',') {
+        break;
+      }
+
+      if (c === '{' || c === '[') {
+        deep++;
+      }
+      if (c === '}' || c === ']') {
+        deep--;
+      }
+
+      index++;
+      if (!~[ ' ', '\n', '\t', '{', '}', '[', ']' ].indexOf(c) && deep >= 0) {
+        lastMeaningfulIndex = index;
+      }
+    }
+    index --;
+    if (!~[ ' ', '\n', '\t', '{', '}', '[', ']' ].indexOf(code.charAt(index)) && deep >= 0) {
+      lastMeaningfulIndex = index;
+    }
+
+    find = find.substr(0, lastMeaningfulIndex - start);
   });
 
+  codeIndex++;
+
   return {
-    start: findLineCharFromIndex(code, index),
-    end: findLineCharFromIndex(code, index + find.length)
+    start: findLineCharFromIndex(code, codeIndex),
+    end: findLineCharFromIndex(code, codeIndex + find.length - 1)
   };
 };
 
@@ -52,7 +87,7 @@ display.writeError = (code, { error, value, parseErrorSchema, parseErrorJson }) 
     statusDom.classList.remove('valid');
     statusDom.classList.add('error');
 
-    while(jsonEditorMarkers.length){
+    while (jsonEditorMarkers.length) {
       jsonEditorMarkers.pop().clear();
     }
 
